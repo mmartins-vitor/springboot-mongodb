640916679247b8b120b68022d058ab80eaec3526
mmartins-vitor <mmartins.vitor@hotmail.com>
2023-12-26 10:09:02 -0300

    criacao do projeto

---
+++ src/main/java/com/vitor/springmongo/SpringmongApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitor/springmongo/SpringmongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
